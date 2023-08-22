b9c3126094a37df5a4ce7eac2ad4d0bf037056b3
Hassan-AlHashim <hwhashim@mit.edu>
2023-08-22 12:24:59 -0400

    first commit

--- test.py
+++ test.py
@@ -2,4 +2,11 @@ msg = "Hello World!!"
 print (msg)
 
 
-#test
+#a function that will ask for two numbers and then it will add them together and print the sum
+def add_two_numbers():
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    sum = num1 + num2
+    print("The sum is: ", sum)
+
+add_two_numbers()
