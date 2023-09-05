5db87954aab31be3229034d4485abc8130df9e46
Hassan Al Hashim <142545009+Hassan-AlHashim@users.noreply.github.com>
2023-09-04 23:08:57 -0400

    2nd commit

--- Sin_function_plt.py
+++ Sin_function_plt.py
@@ -1,8 +1,27 @@
-#plot random x,y coordinates
 import matplotlib.pyplot as plt
-import random
 import numpy as np
+
+# Create a figure and axis with a gray background
+fig, ax = plt.subplots()
+ax.set_facecolor('#2B2B2B')  # Grayish background color
+fig.patch.set_facecolor('#2B2B2B')  # Match the figure color to the axis color
+
 x = np.linspace(0, 10, 1000)
 y = np.sin(x)
-plt.plot(x,y)
+
+# Plot the data with a cyan line
+ax.plot(x, y, color='cyan', linewidth=2)
+
+# Set title and labels for axes with white color
+ax.set_title("Beautiful Sine Wave on Gray Background", color='white')
+ax.set_xlabel("X", color='white')
+ax.set_ylabel("sin(X)", color='white')
+
+# Set the tick colors to white
+ax.tick_params(colors='white')
+
+# Add gridlines
+ax.grid(True, linestyle='--', linewidth=0.5, color='white', alpha=0.6)
+
+# Display the plot
 plt.show()
